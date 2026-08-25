4e39a34a143b288ce0aeda538b603bdb470b5970
Pranav Sodha <pranavsodha01@gmail.com>
2026-08-25 19:36:35 +0530

    Changed COndition Part

--- Logic_Box.py
+++ Logic_Box.py
@@ -17,20 +17,19 @@ elif(choice==2):
     num1=int(input("Enter The Start Of The Range:"))
     
     num2=int(input("Enter The End Of The Range:"))
+    if num1<num2:
+        for i in range(num1,num2+1):
+            if(i%2==0):
+                print(f"Number {i} Is Even.")
+            else:
+                print(f"Number {i} Is Odd.")
+        j=0
+        for i in range(num1,num2+1):
+            j+=i
+        print(f"Sum Of All Numbers From {num1} to {num2} Are: {j}")
+    else: 
+        print("Invalid Input.......")
     
-    for i in range(num1,num2+1):
-        if(i%2==0):
-            print(f"Number {i} Is Even.")
-        else:
-            print(f"Number {i} Is Odd.")
-    
-
-    j=0
-    for i in range(num1,num2+1):
-        j+=i
-    print(f"Sum Of All Numbers From {num1} to {num2} Are: {j}")
-
-
 elif(choice==3):
     
     print("Exiting The Program, Good Bye!")
